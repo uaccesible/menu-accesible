bd699c0fa3b2a07e295ba0da85ce6ef6395304b7
jmfdz <josema.fernandez@ua.es>
2019-06-16 10:41:09 +0200

    Subiendo cambios

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # menu-accesible
 Menú accesible responsive con HTML5 y CSS3
+
+[Demostración](https://github.io/jmfdz/menu-accesible)
--- menu-accesible.js
+++ menu-accesible.js
@@ -1,6 +1,24 @@
 $(document).ready(function(){
-  
-  $("button#button-menu").on("click", function(){
+
+    $("a.accion-desplegable, button.accion-desplegable").on("click", function() {
+        var accionDesplegable = $(this);
+        var elementoDesplegable = $(accionDesplegable.attr("data-desplegable"));
+        if (accionDesplegable.hasClass("contraido")) {
+            accionDesplegable.removeClass("contraido");
+            accionDesplegable.attr("aria-expanded", "true");
+
+            elementoDesplegable.removeClass("oculto");
+            //elementoDesplegable.attr("aria-expanded","true");
+        } else {
+            accionDesplegable.addClass("contraido");
+            accionDesplegable.attr("aria-expanded", "false");
+
+            elementoDesplegable.addClass("oculto");
+            //$("#ul-menu").attr("aria-expanded","false");
+        }
+    });
+
+    /*$("button#button-menu").on("click", function(){
     var obj = $(this);
     if ( obj.hasClass("contraido") ) {
        obj.removeClass("contraido");
@@ -15,7 +33,7 @@ $(document).ready(function(){
       $("#ul-menu").addClass("myhidden");           
       $("#ul-menu").attr("aria-expanded","false");
     }
-  });
+  });*/
 
   /*$("ul#ul-menu li a").on("click",function(){    
     if ( !$("button#button-menu").hasClass("collapsed") ){
